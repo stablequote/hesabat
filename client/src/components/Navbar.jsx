9fa7393a5c906ffe892bf60e0adb70dc89a40592
stablequote <esat.trem@gmail.com>
2026-05-19 15:19:38 +0200

    navbar routes  re-arrangement

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import { Button, Container, Divider, Stack } from '@mantine/core';
+import { Box, Button, Center, Container, Divider, Image, Stack } from '@mantine/core';
 import { useMediaQuery } from '@mantine/hooks';
 import { NavLink } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { IconHome } from '@tabler/icons-react';
 import { useAuth } from '../context/AuthContext'
+import Logo from '../assets/hero.png'
 
 const AppNavbar = () => {
 
@@ -22,11 +23,19 @@ const AppNavbar = () => {
   const role = user?.role;
 
   return (
-    <Stack width={!isMobile ? { base: 250 } : 100} p="xs" sx={{ background: '#1D242E' }}>
-      <NavLink to="/overview" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-        {t("OVERVIEW")}
-      </NavLink>
+    <Stack width={!isMobile ? { base: 250 } : 100} p="xs" style={{ background: '#0e356d' }}>
+        {/* logo */}
+        {/* <Box mb={10} style={{width: 60, height: 30}}>
+            <Image src={Logo} alt='logo' width={60} height={30} />
+        </Box>
+        <Divider /> */}
 
+      {(role === "owner") && (
+         <NavLink to="/overview" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+          {t("OVERVIEW")}
+        </NavLink>
+      )}
+     
       <NavLink to="/clients" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
         {t("CLIENTS")}
       </NavLink>
@@ -35,24 +44,28 @@ const AppNavbar = () => {
         {t("VENDORS")}
       </NavLink>
 
+      <Divider size={1} my={10} />
+
       <NavLink to="/sale-invoices" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
         {t("SALE-INVOICES")}
       </NavLink>
-      
-      <NavLink to="/purchase-invoices" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-        {t("PURCHASE-INVOICES")}
-      </NavLink>
 
-      <NavLink to="/ledger" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-        {t("LEDGER")}
-      </NavLink>
+      {(role === "owner") && (
+        <NavLink to="/purchase-invoices" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+          {t("PURCHASE-INVOICES")}
+        </NavLink>
+      )}
+
+      <Divider size={1} my={10} />
+
+      {(role === "owner") && (
+        <NavLink to="/ledger" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+          {t("LEDGER")}
+        </NavLink>
+      )}
 
       <NavLink to="/expenses" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
         {t("EXPENSES")}
-      </NavLink>
-
-      <NavLink to="/products" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-        {t("PRODUCTS")}
       </NavLink>
 
       <NavLink to="/cashout" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
@@ -60,15 +73,9 @@ const AppNavbar = () => {
       </NavLink>
 
       <Divider size={1} my={10} />
-
-      {(role === "owner" || role === "manager") && (
-        <NavLink to="/orders" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-          {t("ORDERS")}
-        </NavLink>
-      )}
-
-      <NavLink to="/sales" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
-        {t("SALES")}
+      
+      <NavLink to="/products" style={({ isActive }) => (isActive ? activeStyle : undefined)}>
+        {t("PRODUCTS")}
       </NavLink>
 
       <Divider size={1} my={10} />
