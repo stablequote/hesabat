3b83d7c7cf7fab63290cfced4b4d063a73adcd7e
stablequote <esat.trem@gmail.com>
2026-05-19 14:48:18 +0200

    added authorization middleware roles in purchaseInvoice route

--- server/routes/purchaseInvoice.route.js
+++ server/routes/purchaseInvoice.route.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const purchaseInvoice = require('../controllers/purchaseInvoice.controller');
 const authMiddleware = require("../middleware/auth.middleware");
 
-router.post('/create', authMiddleware(["owner", "manager"]), purchaseInvoice.createPurchaseInvoice);
-router.get('/list', purchaseInvoice.getPurchaseInvoices);
-router.post('/payments/:invoiceID/pay', purchaseInvoice.addInstallmentPayment);
-router.put('/edit/:invoiceID', purchaseInvoice.editPurchaseInvoice);
-router.get('/list/vendorID', purchaseInvoice.getPurchaseInvoicesByVendor);
-router.delete('/delete/:invoiceID', purchaseInvoice.deletePurchaseInvoice);
+router.post('/create', authMiddleware(["owner"]), purchaseInvoice.createPurchaseInvoice);
+router.get('/list', authMiddleware(["owner"]), purchaseInvoice.getPurchaseInvoices);
+router.post('/payments/:invoiceID/pay', authMiddleware(["owner"]), purchaseInvoice.addInstallmentPayment);
+router.put('/edit/:invoiceID', authMiddleware(["owner"]), purchaseInvoice.editPurchaseInvoice);
+router.get('/list/vendorID', authMiddleware(["owner"]), purchaseInvoice.getPurchaseInvoicesByVendor);
+router.delete('/delete/:invoiceID', authMiddleware(["owner"]), purchaseInvoice.deletePurchaseInvoice);
 
 module.exports = router;
