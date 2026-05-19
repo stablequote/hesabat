36c89f52d1d2965ba4c4d73b6fd894d2f02efe5e
stablequote <esat.trem@gmail.com>
2026-05-19 14:51:04 +0200

    added expiryDate to addProduct controller in inventory

--- server/controllers/inventory.controller.js
+++ server/controllers/inventory.controller.js
@@ -4,7 +4,7 @@ const Inventory = require('../models/inventory.model');
 // Add a product
 exports.addProduct = async (req, res) => {
     try {
-        const { product, quantity, unit, unitPurchasePrice, unitSalePrice, location } = req.body;
+        const { product, quantity, unit, expiryDate, unitPurchasePrice, unitSalePrice, location } = req.body;
 
         const addedProduct = new Inventory({
             product,
@@ -19,7 +19,7 @@ exports.addProduct = async (req, res) => {
         await addedProduct.save();
         res.status(201).json({ message: 'Product added successfully.', addedProduct });
     } catch (error) {
-        res.status(500).json({ error: 'Failed to add product.' });
+        res.status(500).json({ error: error.message});
         console.log(error)
     }
 };
@@ -27,7 +27,8 @@ exports.addProduct = async (req, res) => {
 // List all products
 exports.listAllProducts = async (req, res) => {
     try {
-        const products = await Inventory.find({});
+        const products = await Inventory.find({})
+        .populate("product")
         res.status(200).json(products);
     } catch (error) {
         res.status(500).json({ error: 'Failed to list products.' });
