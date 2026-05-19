76f075eca90be8d28c6c2409206dd2b1296fe7f1
stablequote <esat.trem@gmail.com>
2026-05-19 15:18:47 +0200

    imported  and implemented sync button to DashboardHeader

--- client/src/components/DashboardHeader.jsx
+++ client/src/components/DashboardHeader.jsx
@@ -10,27 +10,22 @@ import {
   Select,
   Flex,
   Container,
+  Box,
 } from "@mantine/core";
 import { IconSearch, IconBell, IconUser } from "@tabler/icons-react";
 import moment from "moment";
+import SyncButton from './SyncButton';
 
 const DashboardHeader = ({changeLanguage, value, shiftRemainingTime}) => {
   const theme = useMantineTheme();
   return (
-    <Container height={60}  p="md" sx={{backgroundColor: "#1D242E", color: "white"}} >
+    <Box height={60}  p="md" style={{backgroundColor: "#71a0e2", color: "white"}} >
       <Flex justify="space-between">
-        <Text color="white" fw="bold">Soapy</Text>
-        
-        {/* <Group>
-          <Input
-            placeholder="Search for anything here"
-            icon={<IconSearch size={16} />}
-            sx={{ width: 300 }}
-          />
-        </Group> */}
+        <Text color="orange" fz={32} fw="bold" ff="monospace" style={{ lineHeight: "1rem",  }}>حسابات</Text>
 
         {/* Right side: Date, time, and icons */}
         <Group>
+          <SyncButton />
           <Select
             data={[
               {value: "en", label: "English"},
@@ -51,7 +46,7 @@ const DashboardHeader = ({changeLanguage, value, shiftRemainingTime}) => {
           <Text color="white">{shiftRemainingTime}</Text>
         </Group>
       </Flex>
-    </Container>
+    </Box>
   );
 };
 
